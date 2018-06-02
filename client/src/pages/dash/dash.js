30cb739f95f4812967b97cd5eadd8f3762ba4fcd
laurencantlin <laurenrose5321@gmail.com>
2018-06-01 21:54:41 -0400

    adds button and bulma and materialize

--- client/src/pages/dash/dash.js
+++ client/src/pages/dash/dash.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 // import Bulm
+import {Button, Icon} from 'react-materialize'
+
 class Dash extends Component {
     
     render() {
       return (
-        <div>
-          <a className="button">Button</a>
-
-        </div>
+        <div><Button floating fab='horizontal' icon='mode_edit' className='red' large style={{bottom: '45px', right: '24px'}}>
+        <Button floating icon='insert_chart' className='red'/>
+        <Button floating icon='format_quote' className='yellow darken-1'/>
+        <Button floating icon='publish' className='green'/>
+        <Button floating icon='attach_file' className='blue'/>
+      </Button>   </div>
         // </div>
       );
     }
