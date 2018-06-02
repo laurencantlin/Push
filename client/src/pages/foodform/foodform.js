2da1bedbe9a6f4d01c9af0bab4dd963bdc70fa10
laurencantlin <laurenrose5321@gmail.com>
2018-06-01 21:40:22 -0400

    adds react structure and 3 main pages with routes

---
+++ client/src/pages/foodform/foodform.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+class FoodP extends Component {
+    
+    render() {
+      return (
+        <div>
+          foo
+        </div>
+        // </div>
+      );
+    }
+  }
+  
+  export default FoodP;
+  
---
+++ client/src/pages/pushform/pushform.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+class PushP extends Component {
+    
+    render() {
+      return (
+        <div className="has-text-center">
+          pish
+        </div>
+        // </div>
+      );
+    }
+  }
+  
+  export default PushP;
+  
